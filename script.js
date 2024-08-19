5fb79580f722af3cf1ed52ad114f7391854da01c
Orpilla,FritzJ <fritz.orpilla@gmanmi.com>
2024-08-19 16:59:50 +0800

    chore: pm-assessment

---
+++ script.js
@@ -0,0 +1,65 @@
+var index = {
+
+    arrayFunction: function(array, id) {
+        var _ul = document.createElement("ul");
+        array.forEach(function(items) {
+            var _split = items.split(' ').join(' | ').replaceAll('_', ' ');
+            var listItem = document.createElement("li");
+            var anchor = document.createElement("a");
+            anchor.textContent = _split;
+            anchor.href = items;
+            listItem.appendChild(anchor);
+            _ul.appendChild(listItem);
+        });
+        document.getElementById(id).appendChild(_ul);
+    },  
+    navBar: function() {
+        var nav = ['home', 'invetory', 'specials', 'finance', 'service', 'parts', 'collision_center', 'dealership', 'contact_us', 'map'];
+        this.arrayFunction(nav, "nav-button");
+    },
+    featuresLink: function() {
+        var links = ['Quick_Quote', 'Schedule_Service', 'Get_Pre-Approve', 'Honda_E-Store', 'Collision_Center', 'Employment_Center'];
+        this.arrayFunction(links, "a-collection");
+    },
+    footer: function() {
+        var carArray = ['Accord_Coupe ', 
+                        'Accord_Sedan ', 
+                        'Civic_Coupe ', 
+                        'Civic_GX ', 
+                        'Civic_Hybrid ', 
+                        'Civic_Sedan ', 
+                        'Civic_SI_Coupe ', 
+                        'Civic_SI_Sedan ', 
+                        'Crosstour ', 
+                        'CR-V ', 
+                        'CR-Z ' , 
+                        'Element ', 
+                        'FCX_Clarity ', 
+                        'Fit ', 
+                        'Fit_EV ', 
+                        'Insight ', 
+                        'Odyssey ', 
+                        'Pilot ', 
+                        'Ridgline ', 
+                        'S2000'];
+
+        this.arrayFunction(carArray, "cars");
+    },
+    spinnerWidget: function() {
+        const _spinner = document.querySelector(".spinner");
+
+
+        setTimeout(() =>{
+           _spinner.style.display = "none";
+        }, 1000)
+    },
+    init: function() {
+        this.spinnerWidget();
+        this.navBar();
+        this.featuresLink();
+        this.footer();
+    }
+}
+
+
+index.init();
